a5e9e939637fff96fc530d2805edadec46826404
denis <denispavlenko7610new@gmail.com>
2023-04-29 09:17:05 +0300

    add learned

--- Program.cs
+++ Program.cs
@@ -2,27 +2,47 @@
 {
     class Program
     {
+        static int count = 0;
+        static int learnedCount = 0;
+        const string filePathToWords = "words.txt";
+        const string filePathToLearned = "learnedWords.txt";
+
         static void Main(string[] args)
         {
             List<string> words = new List<string>();
-            int count = 0;
-            string filePath = "words.txt";
+            List<string> learnedWords = new List<string>();
 
-            if (File.Exists(filePath))
+            count = GetAllWords(words, count, filePathToWords);
+            learnedCount = GetAllWords(learnedWords, learnedCount, filePathToLearned);
+
+            count = GetCommands(words, learnedWords, count, filePathToWords, filePathToLearned);
+
+            //Console.WriteLine($"Total words: {count}");
+            Console.ReadLine();
+        }
+
+        private static int GetAllWords(List<string> words, int count, string filePathToWords)
+        {
+            if (File.Exists(filePathToWords))
             {
-                count = GetWords(words, count, filePath);
+                count = GetWords(words, count, filePathToWords).Result;
 
-                Console.WriteLine($"Loaded {words.Count} words.");
-                SaveFile(words, filePath);
+                Console.WriteLine($"Loaded {count} words.");
+                SaveFile(words, filePathToWords);
             }
             else
             {
-                Console.WriteLine($"File '{filePath}' does not exist. Creating new file.");
+                Console.WriteLine($"File '{filePathToWords}' does not exist.");
             }
 
+            return count;
+        }
+
+        private static int GetCommands(List<string> words, List<string> learnedWords, int count, string filePathToWords, string filePathToLearned)
+        {
             while (true)
             {
-                string? input = Console.ReadLine();
+                string? input = Console.ReadLine().Trim();
 
                 if (input?.ToLower() == "q") //quit
                 {
@@ -31,39 +51,68 @@
                 else if (input?.ToLower() == "g") //get
                 {
                     Console.WriteLine(GetRandomWord(words));
+                    input = "";
                     continue;
                 }
                 else if ((input?.ToLower() == "d")) //del
                 {
-                    count = DeleteWord(words, count, filePath);
-
+                    count = DeleteWord(words, count, filePathToWords);
+                    input = "";
                     continue;
                 }
                 else if (input?.ToLower() == "c") //clear
                 {
                     Console.Clear();
+                    continue;
+                }
+                else if (input?.ToLower() == "l")
+                {
+                    Console.WriteLine("What word is learned?");
+                    string word = Console.ReadLine();
+                    TryAddWord(words, learnedWords, count, word, true);
+                    input = "";
+                    continue;
                 }
 
-                if (words.Contains(input?.ToLower()))
-                {
-                    Console.WriteLine($"'{input}' already exists.");
-                }
-                else
-                {
-                    count = AddWord(words, count, filePath, input);
-                }
+                count = TryAddWord(words, learnedWords, count, input, false);
             }
 
-            Console.WriteLine($"Total words: {count}");
-            Console.ReadLine();
+            return count;
         }
 
-        private static int AddWord(List<string> words, int count, string filePath, string? input)
+        private static int TryAddWord(List<string> words, List<string> learnedWords, int count, string? word, bool isLearnedWord)
+        {
+            if (isLearnedWord && words.Contains(word.ToLower()))
+            {
+                DeleteWord(filePathToWords, word, words);
+                learnedCount = AddWord(learnedWords, learnedCount, filePathToLearned, word, isLearnedWord);
+                Console.WriteLine("Deleted");
+                return 0;
+            }
+
+            if (words.Contains(word.ToLower()) || learnedWords.Contains(word.ToLower()))
+            {
+                Console.WriteLine($"'{word}' already exists.");
+            }
+            else
+            {
+                count = AddWord(words, count, filePathToWords, word, isLearnedWord);
+            }
+
+            return count;
+        }
+
+        private static int AddWord(List<string> words, int count, string filePath, string input, bool isLearnedWord)
         {
             words.Add(input.ToLower());
-            count++;
             Console.WriteLine($"'{input}' added.");
-            Console.WriteLine($"Total words: {count}");
+
+            if (!isLearnedWord)
+            {
+                count++;
+                Console.WriteLine($"Total words: {count}");
+            }
+
             File.AppendAllTextAsync(filePath, $"{input}\n");
             return count;
         }
@@ -74,13 +123,13 @@
             string worldToDelete = Console.ReadLine();
             if (worldToDelete != null)
             {
-                if (DeleteWord(filePath, worldToDelete))
+                if (DeleteWord(filePath, worldToDelete, words))
                 {
                     count--;
                     Console.WriteLine($"Total words: {count}");
                     words.Clear();
                     count = 0;
-                    count = GetWords(words, count, filePath);
+                    count = GetWords(words, count, filePath).Result;
                 }
                 else
                 {
@@ -91,9 +140,9 @@
             return count;
         }
 
-        private static int GetWords(List<string> words, int count, string filePath)
+        private static async Task<int> GetWords(List<string> words, int count, string filePath)
         {
-            string[] fileWords = File.ReadAllLines(filePath);
+            string[] fileWords = await File.ReadAllLinesAsync(filePath);
             foreach (string word in fileWords)
             {
                 if (!string.IsNullOrEmpty(word))
@@ -113,16 +162,17 @@
             return words[index];
         }
 
-        public static bool DeleteWord(string filePath, string wordToDelete)
+        public static bool DeleteWord(string filePath, string wordToDelete, List<string> listOfWords)
         {
             string[] lines = File.ReadAllLines(filePath);
             bool isDeleted = false;
             for (int i = 0; i < lines.Length; i++)
             {
-                if (lines[i].ToLower() == wordToDelete.ToLower())
+                if (lines[i].ToLower().Contains(wordToDelete.ToLower()))
                 {
                     lines[i] = lines[i].Replace(wordToDelete, "");
                     isDeleted = true;
+                    listOfWords.Remove(lines[i]);
                     Console.WriteLine("Del");
                 }
             }
